1033af727b6c7560bf7b22fd55aea72014a06677
Efthimis <efthimis.koutz@gmail.com>
2024-01-27 21:57:11 +0100

    final

--- src/icon.js
+++ src/icon.js
@@ -1,13 +1,13 @@
 const Icon = () => {
   return (
     <>
-      <p className="par">follow us on facebook</p>
       <a
         href="https://www.facebook.com/groups/5062392140551259"
         className="nav-link"
         target="_blank"
         rel="noreferrer"
       >
+        follow us on facebook
         <i class="fa-brands fa-facebook"></i>
       </a>
     </>
